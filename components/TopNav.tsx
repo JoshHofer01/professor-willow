4d77661e8794b3e852e6e83071e94cfbc81f23ca
Josh H <146031075+JoshHofer01@users.noreply.github.com>
2025-11-24 12:47:05 +0000

    Remove asChild Link
    Extra Link component was causing rendering issues in build

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 const TopNav = () => {
   return (
-    <NavigationMenu className="bg-sidebar pt-1 pb-2">
+    <NavigationMenu className="bg-sidebar pt-1 pb-2 md:hidden">
       <NavigationMenuItem>
         <NavigationMenuLink asChild>
           <Link href="/">
@@ -24,13 +24,10 @@ const TopNav = () => {
         {pages.map((page) => (
           <NavigationMenuItem key={page.title}>
             <NavigationMenuLink
-              asChild
+              href={page.href}
               className="mx-1 font-semibold flex-row data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50"
             >
-              <Link href={page.href}>
-                {/* {page.icon ? <page.icon /> : null} */}
-                {page.title}
-              </Link>
+              {page.title}
             </NavigationMenuLink>
           </NavigationMenuItem>
         ))}
