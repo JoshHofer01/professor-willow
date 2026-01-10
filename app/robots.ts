69c15ee971d03c6c5811c94f03ba65ecb8981010
Josh H <josh@securesap.com>
2026-01-10 12:32:31 +0000

    Fix robots.ts
    Fix malformed Disallow rules

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: ["/"],
-      disallow: ["/search?q=", "/admin/", "settings"]
+      disallow: ["/search?q=", "/admin/", "/settings"]
     },
     sitemap: ["https://professorwillow.me/sitemap.xml"]
   };
