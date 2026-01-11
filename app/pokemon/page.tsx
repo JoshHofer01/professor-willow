07dc50e9ccf6ab3c59fb85ad3cb271c1694ec788
Josh H <146031075+JoshHofer01@users.noreply.github.com>
2026-01-11 13:37:42 +0000

    Refactor fetch & routing for Pokedex
    - [pokemonId] changed from Client render to SSR to allow for next Metadata function to be used.
    - PokedexDisplay changed to client component to accomodate added functionality (seen below)
    - useState hooks along with data retrieval and filtering moved from Pokedex page into PokedexDisplay page
    - Props removed from PokedexDisplay

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,37 +1,23 @@
-"use client";
-
 import { PokedexDisplay } from "@/components/PokemonGroup/PokedexDisplay";
-import pokemonData from "@/data/pokemonIndex.json";
-import { PokemonDataMin } from "@/interfaces/interfaces";
-import { FilterBox } from "@/components/ClientComponents/SearchFilterBox";
-import { useState } from "react";
+import { Metadata } from "next";
 
-function filterPokedex(
-  data: PokemonDataMin[],
-  filterType: string,
-  filterGeneration: string
-) {
-  if (filterType && filterGeneration) {
-    return data.filter(
-      (p) =>
-        (p.primaryType === filterType || p.secondaryType === filterType) &&
-        p.generation.toString() === filterGeneration
-    );
-  } else if (filterType && !filterGeneration) {
-    return data.filter((p) => p.primaryType === filterType);
-  } else if (!filterType && filterGeneration) {
-    return data.filter((p) => p.generation.toString() === filterGeneration);
-  }
-  return data;
-}
+export const metadata: Metadata = {
+  title: "Pokedex | ProfessorWillow",
+  description: "professorwillow.me - View pokedex of Pokemon available in Pokemon GO",
+  keywords: [
+    "pokemon go",
+    "pokemon go pokedex",
+    "pokedex",
+    "professor willow",
+  ],
+  openGraph: {
+    url: "https://professorwillow.me/pokemon",
+    type: "website",
+    title: "Pokedex | ProfessorWillow",
+  },
+};
 
 const Pokemon = () => {
-  const data: PokemonDataMin[] = pokemonData;
-  const [filterGeneration, setFilterGeneration] = useState("");
-  const [filterType, setFilterType] = useState("");
-
-  const pokedex = filterPokedex(data, filterType, filterGeneration);
-
   return (
     <main className="container max-w-full p-4 md:p-6 lg:p-8">
       <div className="mb-8">
@@ -44,14 +30,7 @@ const Pokemon = () => {
           by selecting a Pokemon.
         </p>
       </div>
-      <div className="flex flex-wrap gap-2 mb-4">
-        <p className="text-lg font-semibold">Filter by:</p>
-        <FilterBox dataType="type" setFilter={setFilterType} />
-        <FilterBox dataType="generation" setFilter={setFilterGeneration} />
-      </div>
-      <div className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-2 max-w-[1400px] mx-auto">
-        {pokedex && <PokedexDisplay data={pokedex} />}
-      </div>
+      <PokedexDisplay />
     </main>
   );
 };
--- components/PokemonGroup/PokedexDisplay.tsx
+++ components/PokemonGroup/PokedexDisplay.tsx
@@ -1,34 +1,70 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { PokemonDataMin } from "@/interfaces/interfaces";
 import { weServTransformURL } from "@/utils/weServTransform";
+import { FilterBox } from "../ClientComponents/SearchFilterBox";
+import pokemonData from "@/data/pokemonIndex.json";
+import { useState } from "react";
 
-export const PokedexDisplay = ({ data }: { data: PokemonDataMin[]}) => {
+function filterPokedex(
+  data: PokemonDataMin[],
+  filterType: string,
+  filterGeneration: string
+) {
+  if (filterType && filterGeneration) {
+    return data.filter(
+      (p) =>
+        (p.primaryType === filterType || p.secondaryType === filterType) &&
+        p.generation.toString() === filterGeneration
+    );
+  } else if (filterType && !filterGeneration) {
+    return data.filter((p) => p.primaryType === filterType);
+  } else if (!filterType && filterGeneration) {
+    return data.filter((p) => p.generation.toString() === filterGeneration);
+  }
+  return data;
+}
+
+export const PokedexDisplay = () => {
+  const data: PokemonDataMin[] = pokemonData;
+  const [filterGeneration, setFilterGeneration] = useState("");
+  const [filterType, setFilterType] = useState("");
+  const pokedex = filterPokedex(data, filterType, filterGeneration);
+
   return (
     <>
-      {data.map((pokemon) =>
-        pokemon.image != null ? (
-          <Link
-            href={`/pokemon/${pokemon.dexNr}`}
-            key={pokemon.dexNr}
-            className="max-w-max"
-          >
-            <div className="items-center max-w-28 border-b-2 rounded-lg">
-              <Image
-                src={weServTransformURL(pokemon.image, "pokedexImage")}
-                alt={pokemon.name}
-                width={100}
-                height={100}
-                className="overflow-hidden h-20 object-scale-down p-1"
-              />
-              <div className="text-xs flex flex-col gap-1 m-2 min-w-fit truncate text-center">
-                <p className="text-muted-foreground">#{pokemon.dexNr}</p>
-                <p>{pokemon.name}</p>
+      <div className="flex flex-wrap gap-2 mb-4">
+        <p className="text-lg font-semibold">Filter by:</p>
+        <FilterBox dataType="type" setFilter={setFilterType} />
+        <FilterBox dataType="generation" setFilter={setFilterGeneration} />
+      </div>
+      <div className="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-2 max-w-[1400px] mx-auto">
+        {pokedex.map((pokemon) =>
+          pokemon.image != null ? (
+            <Link
+              href={`/pokemon/${pokemon.dexNr}`}
+              key={pokemon.dexNr}
+              className="max-w-max"
+            >
+              <div className="items-center max-w-28 border-b-2 rounded-lg">
+                <Image
+                  src={weServTransformURL(pokemon.image, "pokedexImage")}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                  className="overflow-hidden h-20 object-scale-down p-1"
+                />
+                <div className="text-xs flex flex-col gap-1 m-2 min-w-fit truncate text-center">
+                  <p className="text-muted-foreground">#{pokemon.dexNr}</p>
+                  <p>{pokemon.name}</p>
+                </div>
               </div>
-            </div>
-          </Link>
-        ) : null
-      )}
+            </Link>
+          ) : null
+        )}
+      </div>
     </>
   );
 };
