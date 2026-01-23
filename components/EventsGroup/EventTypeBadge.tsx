a2befa9711a47244cd42c884737d4f5b23563423
Josh H <josh@securesap.com>
2026-01-23 10:18:49 +0000

    Add global mapping util
    Moved colorMaps.ts into maps.ts to allow for non color related maps to fit into naming convention.

--- components/EventsGroup/EventTypeBadge.tsx
+++ components/EventsGroup/EventTypeBadge.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Badge } from "../shadcn/badge";
-import { eventTypeColorMap } from "@/utils/colorMaps";
+import { eventTypeColorMap } from "@/utils/maps";
 
 const EventTypeBadge = ({
   eventType,
